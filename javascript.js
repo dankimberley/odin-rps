7d3de7e0c3d200d54ec98489d1245f5bdd1a30be
Dan Kimberley <danielkimberley@gmail.com>
2024-03-23 12:38:25 +0000

    added ui to ui branch

--- javascript.js
+++ javascript.js
@@ -11,33 +11,81 @@ function generateRandom(limit) {
     return Math.floor(Math.random() * (limit))
 }
 
-function getUserChoice() {
-    input = prompt('Enter choice: ')
-    input = input.toLowerCase()
-    return input
-}
+const resultText = document.getElementById('result')
 
-function playRound() {
-    user = getUserChoice()
+let userScore = 0
+let compScore = 0
+
+function playRound(user) {
     computer = getComputerChoice()
     if (user === computer) {
+        userScore++
+        compScore++
+        updateScores()
         return "draw"
     } else if (user == "rock" && computer == "scissors") {
+        userScore++
+        updateScores()
         return "rock beats scissors, user wins"
     } else if (user == "rock" && computer == "paper") {
+        compScore++
+        updateScores()
         return "paper beats rock, computer wins"
     } else if (user == 'scissors' && computer == 'rock') {
+        compScore++
+        updateScores()
         return 'rock beats scissors, computer wins'
     } else if (user == 'scissors' && computer == 'paper') {
+        userScore++
+        updateScores()
         return 'scissors beats paper, user wins'
     } else if (user == 'paper' && computer == 'rock') {
+        userScore++
+        updateScores()
         return 'paper beats rock, user wins'
     } else if (user == 'paper' && computer == 'scissors') {
+        compScore++
+        updateScores()
         return 'scissors beats paper, computer wins'
     }
 }
 
-function playGame(rounds) {
+const userBox = document.getElementById('user-score')
+const compBox = document.getElementById('comp-score')
+
+function updateScores() {
+    if (userScore == 5) {
+        resultText.textContent = 'User wins! Play again'
+        userScore = 0
+        compScore = 0
+    } else if (compScore == 5) {
+        resultText.textContent = 'Computer wins! Play again'
+        userScore = 0
+        compScore = 0
+    }
+    userBox.textContent = `User: ${userScore}`
+    compBox.textContent = `Computer: ${compScore}`
+    
+}
+
+
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id)
+    })
+})
+
+
+
+function announceScore(res) {
+    console.log(res)
+    resultText.textContent = res
+}
+
+
+/* function playGame(rounds) {
     let userCount = 0
     let compCount = 0
     for (let i = 0; i < rounds; i++) {
@@ -60,5 +108,4 @@ function playGame(rounds) {
     }
     return
 }
-
-playGame(5)
+ */
